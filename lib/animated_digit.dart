8a5d20306c4201ec51eeceba12e8bd25da53b345
Pratiksha.Kulkarni <Jio@12123>
2024-04-15 15:17:26 +0530

    separator changes

--- lib/animated_digit.dart
+++ lib/animated_digit.dart
@@ -22,7 +22,7 @@ class AnimatedNumber extends StatelessWidget {
       curve: Curves.easeOutCubic,
       duration: const Duration(milliseconds: 400),
       enableSeparator: true,
-      separateSymbol: "ty",
+      separateSymbol: ",",
       fractionDigits: 0,
     );
   }
